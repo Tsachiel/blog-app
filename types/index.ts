eab52ee2510cfff10e516b27df7e4768b0671050
Tsachi <tsachi@gist.com>
2025-01-30 10:44:25 +0200

    Add a type for the fetched posts

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export type PostType = {
+    id: string; // url prop will be used as the id of each post
+    title: string;
+    description: string;
+    url: string;
+    publishedAt: string;
+    content: string;
+    source: string;
+  };
