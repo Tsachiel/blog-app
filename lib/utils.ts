6f8052178c1d4c334bb99f0019f25f1b124d8974
Tsachi <tsachi@gist.com>
2025-01-30 10:21:34 +0200

    Add a function to format the date and time

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+export function formatDateTime(dateString: string): string {
+    if (!dateString) return "Unknown Date"; 
+  
+    const date = new Date(dateString);
+  
+    const formattedDate = date.toISOString().split("T")[0];
+  
+    const hours = date.getUTCHours().toString().padStart(2, "0");
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+    const formattedTime = `${hours}:${minutes}`;
+  
+    return `${formattedDate}, ${formattedTime}`;
+  }
+  
